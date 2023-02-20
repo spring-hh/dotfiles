0b009cff41cd2f9d5b81bb204d83e50e3b0086a1
spring-hh <spring.hh.shu@gmail.com>
2023-02-20 21:51:50 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 if [ ! -d .backup ]; then
   mkdir .backup && mv ~/.zshrc ~/.helpfile .backup
+  ln -s ~/dotfiles/.zshrc ~
+  ln -s ~/dotfiles/.helpfile ~
+  ln -s ~/dotfiles/.vimrc ~
 fi
-
-ln -s ~/dotfiles/.zshrc ~
-ln -s ~/dotfiles/.helpfile ~
-ln -s ~/dotfiles/.vimrc ~
