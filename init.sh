70b0bbf3a89f8f697612e621126706e5a20993cd
spring-hh <spring.hh.shu@gmail.com>
2023-02-20 21:40:47 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -1,25 +1,7 @@
 #!/bin/zsh
-echo "1. ~/dotfiles にいることを確認"
-if [ `pwd` = "$HOME/dotfiles" ]; then
-  echo "[OK]"
-fi
-echo ""
-
-echo "2. 現在の設定のバックアップ"
 rm -rf .backup
 mkdir .backup && mv ~/.zshrc ~/.helpfile .backup
-echo "[OK]"
-echo ""
 
-echo "3. シンボリックリンク作成"
 ln -s ~/dotfiles/.zshrc ~
 ln -s ~/dotfiles/.helpfile ~
 ln -s ~/dotfiles/.vimrc ~
-echo "[OK]"
-echo ""
-
-echo "4. git-secretsを設定"
-git secrets --register-aws --global
-echo "[OK]"
-
-mkdir ~/.nvm
